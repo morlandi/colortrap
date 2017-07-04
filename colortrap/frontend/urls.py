96d14f2f361c7ea4c651bc3dbc28b22c6ba9430a
Andrea Orlandi <andrea.orlandi@brainstorm.it>
2017-07-04 21:31:52 +0200

    Uuid generator

--- colortrap/frontend/urls.py
+++ colortrap/frontend/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from .views import homepage
 from .views import contacts
 from .views import about
+from .views import uuid
 
 urlpatterns = [
     url(r'^$', homepage),
     url(r'^contacts/$', contacts),
     url(r'^about/$', about),
+    url(r'^uuid/$', uuid),
 ]
