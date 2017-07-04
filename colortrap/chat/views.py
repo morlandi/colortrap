450915fdabd97d5066926da97cd5530da1066cfc
Andrea Orlandi <andrea.orlandi@brainstorm.it>
2017-07-04 22:37:27 +0200

    Chat messages listing + new message submit form

--- colortrap/chat/views.py
+++ colortrap/chat/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from .models import Message
+from django.views.decorators.csrf import csrf_exempt
+import ipdb
 
 # Create your views here.
+
+@csrf_exempt
 def index(request):
+
+    if request.method == 'POST':
+    	subject = request.POST['subject']
+    	body = request.POST['body']
+
+    	Message.objects.create(
+    		subject=subject,
+    		body=body,
+    	)
+
+    else:
+    	pass
+
     return render(request, "chat/index.html", {
-    	'messages': [1, 2, 3, 4, ]
+    	'messages': Message.objects.all().order_by('-date'),
     })
 
