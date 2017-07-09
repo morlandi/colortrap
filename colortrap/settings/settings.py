a5b1962acd36844be93034f5e3321bd736277a15
Mario Orlandi <morlandi@brainstorm.it>
2017-07-09 19:14:20 +0200

    static and media root setting

--- colortrap/settings/settings.py
+++ colortrap/settings/settings.py
@@ -121,4 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'public', 'media'))
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'public', 'static'))
+
