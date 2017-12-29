b49d2bcb0d599a62402f5bf350d1f15b9f7b0ff5
Mario Orlandi <morlandi@brainstorm.it>
2017-12-29 11:39:00 +0100

    Sample grid: show calibration and position columns

--- samples/models.py
+++ samples/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 import uuid
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Calibration(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, null=False, blank=False, editable=False)
     date = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=64, null=False, blank=True)
     notes = models.TextField(null=False, blank=True)
+
+    def __str__(self):
+        return self.description
 
 # Create your models here.
 class Sample(models.Model):
