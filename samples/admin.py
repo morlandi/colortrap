6ac91820c3406a6479f864d633d7958a4dbc14ae
Mario Orlandi <morlandi@brainstorm.it>
2017-12-29 09:59:21 +0100

    New Calibration table

--- samples/admin.py
+++ samples/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Sample
-# Register your models here.
+
+
 @admin.register(Sample)
 class SampleAdmin(admin.ModelAdmin):
-
-	list_display = ['date', 'value', ]
-	date_hierarchy = 'date'
+    list_display = ['date', 'value', 'calibration', 'position', ]
+    list_editable = ['value', 'calibration', 'position', ]
+    date_hierarchy = 'date'
--- samples/models.py
+++ samples/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 import uuid
+
+
+class Calibration(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, null=False, blank=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=64, null=False, blank=True)
+    notes = models.TextField(null=False, blank=True)
+
 # Create your models here.
 class Sample(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, null=False, blank=False, editable=False)
     date = models.DateTimeField(auto_now_add=True)
     #value = models.FloatField(default=0.0)
     value = models.CharField(max_length=16, null=False, blank=True)
+    calibration = models.ForeignKey(Calibration, null=True, blank=True)
+    position = models.IntegerField(null=True, blank=True)
